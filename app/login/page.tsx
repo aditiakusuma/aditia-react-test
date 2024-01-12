9a087ac892bd547a5f3af3efb56e0a344af12f5d
aditiakusuma <aditiakusuma800@gmail.com>
2024-01-12 15:44:10 +0700

    redirect login to home page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Page() {
     return (
         <div className="flex justify-center items-center h-screen">
@@ -31,12 +33,14 @@ export default function Page() {
                         required
                     />
                 </div>
+                <Link href="/">
                 <button
                     type="submit"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     Submit
                 </button>
+                </Link>
             </form>
         </div>
     );
