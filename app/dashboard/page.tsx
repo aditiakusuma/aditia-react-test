d0d2fc387ef37d1046a5e101cc2baa22948a91db
aditiakusuma <aditiakusuma800@gmail.com>
2024-01-14 01:06:39 +0700

    add getData in page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,18 @@
+export const getData = () => {
+  return [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' },
+  ];
+};
+
 export default function Home() {
+  const items = getData()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dasboard
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {
+        items.map(x => (<div key={x.id}>{x.name}</div>))
+      }
     </main>
   )
 }
