a34caa194065d0d65a6c3932cd8dbaadb5850fe5
aditiakusuma <aditiakusuma800@gmail.com>
2024-01-14 16:16:00 +0700

    make a list

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,12 @@ import { getData } from "./getData"
 export default function Home() {
   const items = getData()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {
-        items.map(x => (<div key={x.id}>{x.name}</div>))
-      }
+    <main className="flex min-h-screen h-[200vh] p-24">
+      <ol className="list-disc">
+        {
+          items.map(x => (<li key={x.id}>{x.name}</li>))
+        }
+      </ol>
     </main>
   )
 }
